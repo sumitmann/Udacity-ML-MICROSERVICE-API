3edce43f14309bfecb6dd6086621ce831acaf94e
sumit mann <sumitmann93@gmail.com>
2020-10-18 03:03:20 +0530

    upload docker to hub

--- upload_docker.sh
+++ upload_docker.sh
@@ -5,11 +5,14 @@
 
 # Step 1:
 # Create dockerpath
-# dockerpath=<your docker ID/path>
+dockerpath=sumitmann/udacity
 
 # Step 2:  
 # Authenticate & tag
 echo "Docker ID and Image: $dockerpath"
+docker login
+docker tag a0a7a7147ea6 $dockerpath:latest
 
 # Step 3:
 # Push image to a docker repository
+docker push $dockerpath:latest
